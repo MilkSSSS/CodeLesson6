0fa35a3a682bf331f34ee8b593e35cb40571d6ca
Mila <milks0593@gmail.com>
2023-03-02 14:51:06 +0300

    ClassWorkTask3

---
+++ ClassWork_Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+// Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Write("Enter the number of Fibonacci numbers: ");
+int n = int.Parse(Console.ReadLine());
+
+int a = 0;
+int b = 1;
+int c;
+
+Console.Write(a + " " + b + " ");
+
+for (int i = 2; i < n; i++)
+{
+c = a + b;
+a = b;
+b = c;
+
+Console.Write(c + " ");
+}
+
+Console.WriteLine();
