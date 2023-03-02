f866fdf5b877f62911781c042ff74de3142a46c0
Mila <milks0593@gmail.com>
2023-03-02 12:51:07 +0300

    ClassWork_Task2

---
+++ ClassWork_Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int numb = Convert.ToInt32(Console.ReadLine());
+while (numb > 0)
+{
+    Console.Write(numb % 2);  
+    numb = numb / 2;
+}
+Console.WriteLine();
